9197469386596475ecc87a4c7477922be8c2ddc5
Mahir Yıldızhan <mahiryildizhan.com@gmail.com>
2026-04-23 22:17:07 +0300

    Footer correction.

--- resources/views/components/footer-credit.blade.php
+++ resources/views/components/footer-credit.blade.php
@@ -1,3 +1,3 @@
 <p class="text-center text-[10px] text-gray-400 dark:text-gray-600 py-2">
-    Powered by <a href="https://myteknoloji.com.tr" target="_blank" rel="noopener" class="hover:underline">MY Teknoloji</a>
+    Powered by <a href="https://myteknoloji.com" target="_blank" rel="noopener" class="hover:underline">MY Teknoloji</a>
 </p>
